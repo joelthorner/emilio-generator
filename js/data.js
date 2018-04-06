55b7de6d989bc69073f98d326dcfe4ce27546499
Joel Torner <joeltorner@gmail.com>
2018-04-06 11:09:02 +0200

    Add languages id
    All common languages (no LC)

--- js/data.js
+++ js/data.js
@@ -15,6 +15,7 @@ DATA.mails = {
 	23	: "Blog - Notificación de Nuevo Comentario",
 	4	: "Cambio de contraseña",
 	10	: "Confirmación de pedido",
+	25	: "Confirmar suscripción de Stock",
 	9	: "Consulta de producto",
 	8	: "Contacto general",
 	18	: "Cuenta activada",
@@ -31,13 +32,24 @@ DATA.mails = {
 	6	: "Recomendación de wishlist",
 	3	: "Recordarios de contraseña",
 	5	: "Registro de newsletter",
+	20	: "Stock disponible",
 	17	: "Verificación de correo",
 };
 
 // All defined languages: ID lang, Inicials en uppercase
+// 06/04/2018 common translated languages (el LC en te més)
 DATA.langs = {
 	1	: "ES",
 	2	: "EN",
 	3	: "CA",
-	// ficar totes les ID
+	4	: "FR",
+	5	: "DE",
+	6	: "ZHO",	// chino tradicional
+	10	: "CHI",
+	11	: "JA",		// japonés
+	13	: "IT",
+	15	: "KO",		// korean
+	17	: "PT",
+	24	: "EU",		// basque
+	// ?	: "LT",
 };
