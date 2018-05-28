e9d2e0a41646d3643f96bd2b6ed53c07d032d3e5
joelthorner <joeltorner@gmail.com>
2018-05-28 17:25:48 +0200

    Move deleteSubscription

--- languages/ko.js
+++ languages/ko.js
@@ -119,13 +119,85 @@ DATA.ko = {
     <a href="%%ecommerceURL%%" style="color: #000;">%%ecommerceName%%</a> 에서 당신을 뵙기를 기대합니다<br><br>%%ecommerceName%%</span>
   </td>
 </tr>
-<tr>
-  <td style="padding: 0 20px 20px 20px;color: #999;font-family: Arial, sans-serif;font-size: 18px;line-height: 20px;text-align:center;">
-    <span style="font-family:Arial, sans-serif;font-size: 11px;color: #999;height:15px;">
-      <a href="%linkDeleteSubscription%" style="color: #333;text-decoration: underline;font-size:10px;line-height:13px;">Unsubscribe</a> <span style="color:#333;font-size:10px;">from the Newsletter</span>
-    </span>
-  </td>
-</tr>
+`,
+    footer : `
+        <tr>
+          <td bgcolor="#E8E8E8" style="padding: 30px 30px 15px 30px;">
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">    
+              <tr>
+                <td align="center" >
+                  <table border="0" cellspacing="10" cellpadding="0">
+                    <tr>
+                      <!-- %%Pages-501-Loop%% -->
+                      <td style="text-align: center;">
+                        <a href="%%pageLink%%" style="padding:6px 10px;color:#454545;font-family:Arial, sans-serif;font-size:14px;font-weight:bold;text-decoration:none;" target="%%pageTarget%%"> %%pageName%% </a>
+                      </td>
+                      <!-- %%/Pages-501-Loop%% -->
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="padding: 12px 0 12px 0px;">
+                  <table border="0" cellspacing="8" cellpadding="0">
+                    <tr>
+                      <!-- %%Banners-502-Loop%% --> 
+                      <td width="25" style="text-align: center; ">
+                        <a href="%%BannerLink%%" target="">
+                          <img src="%%BannerImage%%" width="20" height="auto" alt="%%BannerAlt%%" border="0" style="height: auto;">
+                        </a>
+                      </td>
+                      <!-- %%/Banners-502-Loop%% -->
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" >
+                  <table border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <!-- %%Pages-503-Loop%% --> 
+                      <td style="text-align: center; padding: 15px 25px 25px 25px;color:#999;font-family:Arial, sans-serif;font-size:14px;line-height:20px;">
+                        <span style="color:#999;font-family:Arial, sans-serif;font-size:14px;line-height:20px;">
+                          %%pageContent%%
+                        </span>
+                      </td>
+                      <!-- %%/Pages-503-Loop%% -->
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <!-- %%Pages-504-Loop%% --> 
+                <td align="center" style="font-family:Arial, sans-serif;font-size: 13px;color: #454545;">
+                  %%pageContent%%
+                </td>
+                <!-- %%/Pages-504-Loop%% -->  
+              </tr>
+              <tr>
+                <td align="center" heigth="50" style="font-family:Arial, sans-serif;font-size: 11px;color: #999;height:20px;">
+                  자세한 내용은 %%privacy%% 과 %%termsOfUse%%.
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <!--[if (gte mso 9)|(IE)]>
+            </td>
+          </tr>
+        </table>
+      <![endif]-->
+    </td>
+  </tr>
+  <tr>
+    <td style="padding: 0 20px 20px 20px;color: #999;font-family: Arial, sans-serif;font-size: 18px;line-height: 20px;text-align:center;">
+      <span style="font-family:Arial, sans-serif;font-size: 11px;color: #999;height:15px;">
+        <a href="%linkDeleteSubscription%" style="color: #333;text-decoration: underline;font-size:10px;line-height:13px;">Unsubscribe</a> <span style="color:#333;font-size:10px;">from the Newsletter</span>
+      </span>
+    </td>
+  </tr>
+</table>
 `
   }, // end 1
 
@@ -175,13 +247,85 @@ DATA.ko = {
     <span style="font-size:15px;">안녕하세요<br><br>우리의 뉴스 레터에 구독 해 주셔서 감사합니다.  곧 우리 회사에 대해 뉴스,  제품 기능,인기 제품과 판촉을 받을 것입니다 <br><br><a href="%%ecommerceURL%%" style="color:#000;">%%ecommerceName%%</a> 에서 당신을 뵙기를 기대합니다</span>
   </td>
 </tr>
-<tr>
-  <td style="padding: 0 20px 20px 20px;color: #999;font-family: Arial, sans-serif;font-size: 18px;line-height: 20px;text-align:center;">
-    <span style="font-family:Arial, sans-serif;font-size: 11px;color: #999;height:15px;">
-      <a href="%linkDeleteSubscription%" style="color: #333;text-decoration: underline;font-size:10px;line-height:13px;">Unsubscribe</a> <span style="color:#333;font-size:10px;">from the Newsletter</span>
-    </span>
-  </td>
-</tr>
+`,
+    footer : `
+        <tr>
+          <td bgcolor="#E8E8E8" style="padding: 30px 30px 15px 30px;">
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">    
+              <tr>
+                <td align="center" >
+                  <table border="0" cellspacing="10" cellpadding="0">
+                    <tr>
+                      <!-- %%Pages-501-Loop%% -->
+                      <td style="text-align: center;">
+                        <a href="%%pageLink%%" style="padding:6px 10px;color:#454545;font-family:Arial, sans-serif;font-size:14px;font-weight:bold;text-decoration:none;" target="%%pageTarget%%"> %%pageName%% </a>
+                      </td>
+                      <!-- %%/Pages-501-Loop%% -->
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="padding: 12px 0 12px 0px;">
+                  <table border="0" cellspacing="8" cellpadding="0">
+                    <tr>
+                      <!-- %%Banners-502-Loop%% --> 
+                      <td width="25" style="text-align: center; ">
+                        <a href="%%BannerLink%%" target="">
+                          <img src="%%BannerImage%%" width="20" height="auto" alt="%%BannerAlt%%" border="0" style="height: auto;">
+                        </a>
+                      </td>
+                      <!-- %%/Banners-502-Loop%% -->
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" >
+                  <table border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <!-- %%Pages-503-Loop%% --> 
+                      <td style="text-align: center; padding: 15px 25px 25px 25px;color:#999;font-family:Arial, sans-serif;font-size:14px;line-height:20px;">
+                        <span style="color:#999;font-family:Arial, sans-serif;font-size:14px;line-height:20px;">
+                          %%pageContent%%
+                        </span>
+                      </td>
+                      <!-- %%/Pages-503-Loop%% -->
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <!-- %%Pages-504-Loop%% --> 
+                <td align="center" style="font-family:Arial, sans-serif;font-size: 13px;color: #454545;">
+                  %%pageContent%%
+                </td>
+                <!-- %%/Pages-504-Loop%% -->  
+              </tr>
+              <tr>
+                <td align="center" heigth="50" style="font-family:Arial, sans-serif;font-size: 11px;color: #999;height:20px;">
+                  자세한 내용은 %%privacy%% 과 %%termsOfUse%%.
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <!--[if (gte mso 9)|(IE)]>
+            </td>
+          </tr>
+        </table>
+      <![endif]-->
+    </td>
+  </tr>
+  <tr>
+    <td style="padding: 0 20px 20px 20px;color: #999;font-family: Arial, sans-serif;font-size: 18px;line-height: 20px;text-align:center;">
+      <span style="font-family:Arial, sans-serif;font-size: 11px;color: #999;height:15px;">
+        <a href="%linkDeleteSubscription%" style="color: #333;text-decoration: underline;font-size:10px;line-height:13px;">Unsubscribe</a> <span style="color:#333;font-size:10px;">from the Newsletter</span>
+      </span>
+    </td>
+  </tr>
+</table>
 `
   }, // end 5
 
