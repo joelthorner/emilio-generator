5f27736629e773eac170b7d43f50a141fb28577c
Joel Thorner <joeltorner@gmail.com>
2018-10-16 09:07:34 +0200

    Update diclosure confirm order (id 10)

--- languages/es.js
+++ languages/es.js
@@ -583,18 +583,31 @@ DATA.es = {
                     <td style="text-align: right;padding-right:8px;border-bottom:1px solid #dcdcdc;"><span><span>%price%</span></span></td>
                   </tr>
                   <!-- %/loop% -->
+
+                  <!-- %ifOrderGifts% -->
+                  <!-- %giftsLoop% -->
+                  <tr>
+                    <td style="text-align: left;font-family:Arial, Helvetica, sans-serif;font-size:14px;border-bottom:1px solid #dcdcdc;">
+                      <table style="color: #454545; font-family:sans-serif;font-size:14px;vertical-align: middle;" >
+                        <tr>
+                          <td style="padding: 10px 0px;">
+                            <img width="60" src="%smallImage%" alt="%name%" onerror="this.style.display='none';"> 
+                          </td>  
+                          <td style="padding: 10px 0px;">
+                            <a href=" %productLink%" title="%name%" target="_blank" rel="noreferrer" style="text-decoration:none;"><span style="color: #454545; text-decoration: none">%name%</span></a>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                    <td style="text-align: center;border-bottom:1px solid #dcdcdc;">%quantity%</td>
+                    <td style="text-align: right;padding-right:8px;border-bottom:1px solid #dcdcdc;">&nbsp;</td>
+                  </tr>
+                  <!-- %/giftsLoop% -->
+                  <!-- %/ifOrderGifts% -->
                   
                 </tbody>
               </table>
               <table align="center" cellpadding="0" cellspacing="0" width="88%" style="color:#454545;font-family:Arial, Helvetica, sans-serif;font-size:14px;">
-                <tr>
-                  <td height="32" width="120" style="vertical-align: middle;text-align:left;padding-left:10px; border-bottom:1px solid #dcdcdc;font-weight:bold;">Subtotal</td>
-                  <td height="32" width="75" style="text-align:right;padding-right:10px; border-bottom:1px solid #dcdcdc;">
-                    <span>
-                      <span>%%orderTotalWithoutTaxes%%  </span> 
-                    </span>
-                  </td>
-                </tr>
                 <tr>
                   <td height="32" width="120" style="vertical-align: middle;text-align:left;padding-left:10px; border-bottom:1px solid #dcdcdc;">%%shipperName%%  %%shippingTypeName%%</td>
                   <td height="32" width="75" style="text-align:right;padding-right:10px; border-bottom:1px solid #dcdcdc;">
@@ -622,23 +635,40 @@ DATA.es = {
                 </tr>
                 <!-- %%/ifOrderDiscount%% -->
                 <tr>
-                  <td height="32" width="120" style="vertical-align: middle;text-align:left;padding-left:10px; border-bottom:1px solid #dcdcdc;">
-                    Impuestos
-                  </td>
+                  <td height="32" width="120" style="vertical-align: middle;text-align:left;padding-left:10px; border-bottom:1px solid #dcdcdc;font-weight:bold;">Subtotal</td>
                   <td height="32" width="75" style="text-align:right;padding-right:10px; border-bottom:1px solid #dcdcdc;">
                     <span>
-                      <span>%%orderTaxes%%</span>
+                      <span>%%orderTotalWithoutTaxes%%  </span> 
                     </span>
                   </td>
                 </tr>
+
+                <tr>
+                  <td height="32" width="120" style="vertical-align: middle;text-align:left;padding-left:10px; border-bottom:1px solid #dcdcdc;">Impuestos</td>
+                  <td height="32" width="75" style="text-align:right;padding-right:10px; border-bottom:1px solid #dcdcdc;">
+                    <span>
+                      <span>%%orderTaxes%% </span>
+                    </span>
+                  </td>
+                </tr>
+                <!--%%ifOrderBalanceCodes%%-->
+                <tr>
+                  <td height="32" width="120" style="vertical-align: middle;text-align:left;padding-left:10px; border-bottom:1px solid #dcdcdc;">Vales</td>
+                  <td height="32" width="75" style="text-align:right;padding-right:10px; border-bottom:1px solid #dcdcdc;">
+                    <span>
+                      <span>- %%orderBalanceCodes%%</span>
+                    </span>
+                  </td>
+                </tr>
+                <!--%%/ifOrderBalanceCodes%%-->
                 <tr>
                   <td height="32" width="120" bgColor="#f4f4f4" style="vertical-align: middle;text-align:left;padding-left:10px; border-bottom:1px solid #dcdcdc;font-weight:bold;">Total</td>
                   <td height="32" width="75" bgColor="#f4f4f4" style="text-align:right;padding-right:10px; border-bottom:1px solid #dcdcdc;font-weight:bold;">
                     <span>
-                      <span>%%total%% </span>
+                      <span>%%total%%</span>
                     </span>
                   </td>
-                </tr>   
+                </tr>
                 
               </table>
             </div>
