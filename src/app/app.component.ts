61dd874f41cb62a2398301c2212aec4925f3ad93
joelthorner <joeltorner@gmail.com>
2018-12-31 03:30:29 +0100

    First 2x commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'emilio-generator';
+}
