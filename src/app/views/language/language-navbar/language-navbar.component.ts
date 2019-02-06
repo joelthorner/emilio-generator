af90fe7b90de8f0d6422e87f35a62df2331d4463
joelthorner <joeltorner@gmail.com>
2019-02-06 22:58:12 +0100

    Language search feature

--- src/app/views/language/language-navbar/language-navbar.component.ts
+++ src/app/views/language/language-navbar/language-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppData } from 'src/app/data/app-data';
+import { SearchService } from 'src/app/lib/services/search.service';
 
 @Component({
   selector: 'eg-language-navbar',
@@ -10,21 +11,29 @@ import { AppData } from 'src/app/data/app-data';
 export class LanguageNavbarComponent implements OnInit {
 
   public langKey: string;
+  public searchValue: string;
 
   constructor(
     private appData: AppData,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private search: SearchService
     ) {
 
   }
 
   ngOnInit() {
+    this.search.currentSearch.subscribe(searchValue => this.searchValue = searchValue);
     this.langKey = this.route.snapshot.params['langKey'];
   }
 
-  downloadZipLanguage(event) {
+  downloadZipLanguage(event: any) {
     event.preventDefault();
     this.appData.generateLanguageZip(this.langKey);
+  }
+
+  searchChange(event: any) {
+    event = event.toLowerCase().trim();
+    this.search.changeSearch(event);
   }
 
 }
--- src/app/views/language/language/language.component.ts
+++ src/app/views/language/language/language.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppData } from 'src/app/data/app-data';
+import { SearchService } from 'src/app/lib/services/search.service';
 
 
 @Component({
@@ -15,16 +16,19 @@ export class LanguageComponent implements OnInit {
   public objectKeys = Object.keys;
 
   public previewEmail: any;
+  public searchValue: string;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    public appData: AppData
+    public appData: AppData,
+    private search: SearchService
   ) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   ngOnInit() {
+    this.search.currentSearch.subscribe(searchValue => this.searchValue = searchValue);
     this.langKey = this.route.snapshot.params['langKey'];
     this.langData = this.appData.getLanguage(this.langKey);
     this.previewEmail = this.langData.emails.templates[1];
@@ -60,6 +64,32 @@ export class LanguageComponent implements OnInit {
     } else {
       // add footer
       this.appData.setCustomFooter(this.langKey, emailId);
+    }
+  }
+
+  findedLangItem(item: any, type: string) {
+    let finded = false;
+
+    if (this.searchValue.length > 2) {
+      if (typeof item === 'string') {
+        if (item.toLowerCase().trim().includes(this.searchValue)) {
+          finded = true;
+        }
+      } else {
+        if (item.name.toLowerCase().trim().includes(this.searchValue)) {
+          finded = true;
+        }
+        if (item.subject.toLowerCase().trim().includes(this.searchValue)) {
+          finded = true;
+        }
+      }
+    }
+
+
+    if (type === 'class') {
+      return finded ? 'finded' : '';
+    } else if (type === 'order') {
+      return finded ? 1 : 10;
     }
   }
 }
