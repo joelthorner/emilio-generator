72fccd4967fab8859cd797e5f9a3d4c10fda3d1f
joelthorner <joeltorner@gmail.com>
2019-03-14 21:26:40 +0100

    Fix #63 - Add all languages empty

--- src/app/data/app-data-languages.ts
+++ src/app/data/app-data-languages.ts
@@ -4,6 +4,7 @@ import { LANGUAGE_CA } from './app-language-ca';
 import { LANGUAGE_FR } from './app-language-fr';
 import { LANGUAGE_DE } from './app-language-de';
 import { LANGUAGE_ZHO } from './app-language-zho';
+import { LANGUAGE_AR } from './app-language-ar';
 import { LANGUAGE_CHI } from './app-language-chi';
 import { LANGUAGE_JA } from './app-language-ja';
 import { LANGUAGE_IT } from './app-language-it';
@@ -11,6 +12,17 @@ import { LANGUAGE_KO } from './app-language-ko';
 import { LANGUAGE_PT } from './app-language-pt';
 import { LANGUAGE_EU } from './app-language-eu';
 import { LANGUAGE_SV } from './app-language-sv';
+import { LANGUAGE_RU } from './app-language-ru';
+import { LANGUAGE_MN } from './app-language-mn';
+import { LANGUAGE_BG } from './app-language-bg';
+import { LANGUAGE_RO } from './app-language-ro';
+import { LANGUAGE_NL } from './app-language-nl';
+import { LANGUAGE_HU } from './app-language-hu';
+import { LANGUAGE_PL } from './app-language-pl';
+import { LANGUAGE_SK } from './app-language-sk';
+import { LANGUAGE_TR } from './app-language-tr';
+import { LANGUAGE_TH } from './app-language-th';
+import { LANGUAGE_LO } from './app-language-lo';
 
 export const LANGUAGES = {
   1: {
@@ -43,6 +55,11 @@ export const LANGUAGES = {
     name: 'Trad. Chinese',
     emails: LANGUAGE_ZHO
   },
+  7: {
+    key: 'ar',
+    name: 'Arabic',
+    emails: LANGUAGE_AR
+  },
   10: {
     key: 'chi',
     name: 'Simpl. Chinese',
@@ -52,6 +69,11 @@ export const LANGUAGES = {
     key: 'ja',
     name: 'Japanese',
     emails: LANGUAGE_JA
+  },
+  12: {
+    key: 'ru',
+    name: 'Russian',
+    emails: LANGUAGE_RU
   },
   13: {
     key: 'it',
@@ -68,10 +90,60 @@ export const LANGUAGES = {
     name: 'Portuguese',
     emails: LANGUAGE_PT
   },
+  19: {
+    key: 'ro',
+    name: 'Romanian',
+    emails: LANGUAGE_RO
+  },
+  20: {
+    key: 'nl',
+    name: 'Dutch',
+    emails: LANGUAGE_NL
+  },
+  21: {
+    key: 'hu',
+    name: 'Hungarian',
+    emails: LANGUAGE_HU
+  },
+  22: {
+    key: 'sk',
+    name: 'Slovak',
+    emails: LANGUAGE_SK
+  },
   24: {
     key: 'eu',
     name: 'Basque',
     emails: LANGUAGE_EU
+  },
+  25: {
+    key: 'tr',
+    name: 'Turkish',
+    emails: LANGUAGE_TR
+  },
+  26: {
+    key: 'th',
+    name: 'Thai',
+    emails: LANGUAGE_TH
+  },
+  27: {
+    key: 'lo',
+    name: 'Lao',
+    emails: LANGUAGE_LO
+  },
+  28: {
+    key: 'pl',
+    name: 'Polish',
+    emails: LANGUAGE_PL
+  },
+  31: {
+    key: 'mn',
+    name: 'Mongolian',
+    emails: LANGUAGE_MN
+  },
+  32: {
+    key: 'bg',
+    name: 'Bulgarian',
+    emails: LANGUAGE_BG
   },
   34: {
     key: 'sv',
