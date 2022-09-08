440c7e8e3316f7042eaf39e1ba73c897ca9b0477
Joelthorner <joeltorner@gmail.com>
2022-09-08 17:03:03 +0200

    Fix #127

--- src/app/views/language/modal-preview-settings/modal-preview-settings.component.ts
+++ src/app/views/language/modal-preview-settings/modal-preview-settings.component.ts
@@ -26,7 +26,7 @@ export class ModalPreviewSettingsComponent implements OnInit {
     public appData: AppData,
     public sanitizer: DomSanitizer,
     private route: ActivatedRoute
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.langKey = this.route.snapshot.params['langKey'];
@@ -44,7 +44,7 @@ export class ModalPreviewSettingsComponent implements OnInit {
     this.appData.setPreviewIframeContent(this.langKey);
   }
 
-  onSelectLogoFile(event: any) {
+  selectImageLogic(event: any, file: string) {
     if (event.target.files && event.target.files[0]) {
       let reader: any;
       reader = new FileReader();
@@ -58,46 +58,26 @@ export class ModalPreviewSettingsComponent implements OnInit {
       reader.onload = function (imgsrc: any) {
         if (isImage) {
           if (mb <= 1) {
-            self.logoSrc = self.sanitizer.bypassSecurityTrustResourceUrl(imgsrc.target.result);
-            self.logoSrcBase64 = imgsrc.target.result;
-            self.logoName = event.target.files[0].name;
-            self.logoErrorMsg = '';
+            self[file + 'Src'] = self.sanitizer.bypassSecurityTrustResourceUrl(imgsrc.target.result);
+            self[file + 'SrcBase64'] = imgsrc.target.result;
+            self[file + 'Name'] = event.target.files[0].name;
+            self[file + 'ErrorMsg'] = '';
           } else {
-            self.logoErrorMsg = 'Max file size 1MB!';
+            self[file + 'ErrorMsg'] = 'Max file size 1MB!';
           }
         } else {
-          self.logoErrorMsg = 'Only jpg / jpeg or png files!';
+          self[file + 'ErrorMsg'] = 'Only jpg / jpeg or png files!';
         }
       };
     }
   }
 
+  onSelectLogoFile(event: any) {
+    this.selectImageLogic(event, 'logo');
+  }
+
   onSelectSocialFile(event: any) {
-    if (event.target.files && event.target.files[0]) {
-      let reader: any;
-      reader = new FileReader();
-
-      reader.readAsDataURL(event.target.files[0]);
-
-      const mb = event.target.files[0].size / 1024 / 1024;
-      const isImage = event.target.files[0].type.match('(image/jpeg|image/png)');
-      const self = this;
-
-      reader.onload = function (imgsrc: any) {
-        if (isImage) {
-          if (mb <= 1) {
-            self.socialSrc = self.sanitizer.bypassSecurityTrustResourceUrl(imgsrc.target.result);
-            self.socialSrcBase64 = imgsrc.target.result;
-            self.socialName = event.target.files[0].name;
-            self.socialErrorMsg = '';
-          } else {
-            self.socialErrorMsg = 'Max file size 1MB!';
-          }
-        } else {
-          self.socialErrorMsg = 'Only jpg / jpeg or png files!';
-        }
-      };
-    }
+    this.selectImageLogic(event, 'social');
   }
 
 }
